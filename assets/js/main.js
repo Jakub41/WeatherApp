d409e64ac266d826cad87456d2039f98795d9389
jakub41 <lemiszewski@gmx.com>
2019-11-12 00:12:25 +0100

    Async fecthing API & data

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,15 +22,30 @@ window.onload = () => {
   if (navigator.geolocation) {
     // Getting current position
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
       // Getting coordinates we accessed long/lat
       long = position.coords.longitude;
       lat = position.coords.latitude;
+      // CORS fix
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
+      // API -> we pass as @params 1st lat 2sd long
+      const api = `${proxy}https://api.darksky.net/forecast/6ff3bc141da2e9d546565b4cf31812c6/${lat},${long}`;
+      
+      // Calling the async function
+      // We need to fetch the data from API
+      // We use async function passing our API
+      const getData = async () => {
+          // We fetch and await the response
+          const response = await fetch(api);
+          // We wait the results in a JSON OBJ
+          const data = await response.json();
+          // Short hand to get the data -> temperature & summary
+          const {temperature, summary} = data.currently;
 
-      // API
-      const api = `https://api.darksky.net/forecast/6ff3bc141da2e9d546565b4cf31812c6/37.8267,-122.4233`;
+
+      }   
     });
   } else {
+    // We show a message if the geolocation Doesn’t Success
     h1.textContent =
       "Please check if your browser has permission to lactation or support of it";
   }
